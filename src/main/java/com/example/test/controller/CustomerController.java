c471bcc3d9a08a977c4c431e4088dbe2e1b3703a
shinjaeho <sinj42907@gmail.com>
2026-01-12 14:52:13 +0900

    이름

--- src/main/java/com/example/test/controller/CustomerController.java
+++ src/main/java/com/example/test/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class CustomerController {
     private final CustomerService customerService;
     // GetMapping - customers
+
     @GetMapping("/customers")
     public String customers(Model model){
         model.addAttribute("customers", customerService.getCustomers());
