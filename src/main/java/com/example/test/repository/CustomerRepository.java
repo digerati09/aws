6b94f937e3597d9901c0549c18f767290bd5da54
digerati009 <ijaeyong8954@ gmail.com>
2026-01-12 14:18:49 +0900

    초미니 프로젝트!!

--- src/main/java/com/example/test/repository/CustomerRepository.java
+++ src/main/java/com/example/test/repository/CustomerRepository.java
@@ -2,6 +2,7 @@ package com.example.test.repository;
 
 import com.example.test.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+// repository
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
